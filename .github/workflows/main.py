264c98a61a6e669a7abc78ff4bb2a9bc389ff545
Natan-gal <natangal066@gmail.com>
2026-05-07 14:56:10 +0200

    Delete .github/workflows/main.py

--- .github/workflows/main.py
+++
@@ -1,8 +0,0 @@
-print("oto jakieś zadanie które zrobiłem z użyciem pythona")
-name = "name: A8 \n"
-brand = "brand: Audi\n"
-age = "Age: 12\n"
-colour = "colour: red\n"
-maximum_speed = "maximum speed is: 140" + "km" +  "/" + "h"
-information = "Information about this car: \n" + brand + name +  age + colour + maximum_speed
-print(information)
