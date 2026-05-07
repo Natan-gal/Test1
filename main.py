3e4354a48987197820161aeee640f781551affa0
Natan-gal <natangal066@gmail.com>
2026-05-07 14:59:12 +0200

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ age = "Age: 12\n"
 colour = "colour: red\n"
 maximum_speed = "maximum speed is: 140" + "km" +  "/" + "h"
 information = "Information about this car: \n" + brand + name +  age + colour + maximum_speed
-print(information)
+print(information
